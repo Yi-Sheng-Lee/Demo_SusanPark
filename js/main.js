fc4f5cc95eda14cbe8d59a1eebce6e3df28984d0
Yi-Sheng-Lee <50228624+Yi-Sheng-Lee@users.noreply.github.com>
2019-05-06 12:15:40 +0800

    Add SusanPark

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	var span = $('#header>div>div:nth-of-type(2)>nav>ul>li>div[data-decoration="corner"]>label>span');
+	var h3 = $('#header>div>div:nth-of-type(2)>nav>ul>li>div[data-layout=submenu]>section>label>h3');
+
+	var preObj1='';
+	var preObj2='';
+	
+	span.click(function(){
+		if(preObj1){
+			preObj1.slideUp();
+			//console.log("up");
+		}
+		if(preObj2){
+			preObj2.slideUp();
+			preObj2 = '';
+		} 
+		preObj1 = $(this).parentsUntil('li').nextUntil("ul.submenu");
+		preObj1.slideDown();
+
+	});
+
+	h3.click(function(){
+		console.log("aa");
+		if(preObj2){
+			preObj2.slideUp();
+		}
+		preObj2 = $(this).parentsUntil('section').nextUntil('li');
+		preObj2.slideDown();
+	});
+});
