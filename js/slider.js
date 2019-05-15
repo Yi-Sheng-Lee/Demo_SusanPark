476e61631eaf2ace35f6c831353ccc8ea552ca17
Yi-Sheng-Lee <50228624+Yi-Sheng-Lee@users.noreply.github.com>
2019-05-15 21:51:48 +0800

    Fix slider
    Fix picture slider and timer set

--- js/slider.js
+++ js/slider.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	var arrows = $('#slider>div>section:nth-of-type(2)>a');
 	var points = $('#slider>div>section:nth-of-type(3)>ul a');
 
-	var nRight, nLeft, current, out, i, direction;
-
+	var nRight, nLeft, current, out;
+	var speed = 2500;
 	var timer; //slide timer
 	var pos = 0; //active position
 	var len = slider.length - 1; //slides total number
@@ -13,67 +13,85 @@ $(document).ready(function(){
 	arrows.on({
 		click: function(e){
 			e.preventDefault();
-			direction = $(this).attr("data-direction") == 'right' ? true : false;
-			
-			changePic(direction);
-
+			slider.removeAttr('class');
+			//var direction = $(this).attr("data-direction") == 'right' ? true : false;
+			clearInterval(timer);
+			switch($(this).attr("data-direction")){
+				case 'right':
+					posRight();
+					break;
+				case 'left':
+					posLeft();
+			}
+			setTimer();
 		}
 	});
 
-	function changePic(dir){
-		switch(dir){
-			case true:
-				picRight();
-				break;
-			case false:
-				picLeft();
-		}
-	}
-	function picRight(){
-		out = nLeft.removeAttr('class').addClass('l2O');
-		nLeft = slider.eq(pos++).removeAttr('class').addClass('a2L');
+	function posRight(){
+		slider.removeAttr('class');
+		out = nLeft;
+		nLeft = pos++;
 		if(pos > len){
 			pos = 0;
 		} 
-		current = slider.eq(pos).removeAttr('class').addClass('active r2A');
-		if(pos+1>len){
-			nRight = slider.eq(0).removeAttr('class').addClass('o2R');
-		} else{
-			nRight = slider.eq(pos+1).removeAttr('class').addClass('o2R');
-		}
+		current = pos;
+		nRight = (pos + 1 > len) ? 0 : pos + 1 ; 
+		picRight();
 	}
 
-	function picLeft(){
-		out = nRight.removeAttr('class').addClass('r2O');
-		nRight = slider.eq(pos--).removeAttr('class').addClass('a2R');
+	function posLeft(){
+		out = nRight;
+		nRight = pos--;
 		if(pos < 0){
 			pos = len;
 		} 
-		current = slider.eq(pos).removeAttr('class').addClass('active l2A');
-		if(pos-1 < 0){
-			nLeft = slider.eq(len).removeAttr('class').addClass('o2L');
-		} else{
-			nLeft = slider.eq(pos-1).removeAttr('class').addClass('o2L');
-		}
+		current = pos;
+		nLeft = (pos - 1 < 0) ? len : pos - 1;
+		picLeft();
+	}
+
+	function picRight(){
+		slider.eq(out).addClass('l2O');
+		slider.eq(nLeft).addClass('a2L');
+		slider.eq(current).addClass('active r2A');
+		slider.eq(nRight).addClass('o2R');
+	}
+
+	function picLeft(){
+		slider.eq(out).addClass('r2O');
+		slider.eq(nRight).addClass('a2R');
+		slider.eq(current).addClass('active l2A');
+		slider.eq(nLeft).addClass('o2L');
 	}
 	
 	function initialSlider(){
-		$('#slider>div>section:nth-of-type(1)').height($(this).width() * 0.6 * 0.69 + 16);
+		if(window.matchMedia("(min-width:601px) and (max-width:1280px)").matches){
+			$('#slider>div>section:nth-of-type(1)').height($(this).width() * 0.7 * 0.69 + 16);
+		} else{
+			$('#slider>div>section:nth-of-type(1)').height($(this).width() * 0.6 * 0.69 + 16);
+		}
 	}
 
 	function initial(){
-		current = slider.eq(pos).addClass('active r2A');
-		nRight = slider.eq(pos+1).addClass('o2R');
-		nLeft = slider.eq(len).addClass('o2L');
+		// current = slider.eq(pos).addClass('active r2A');
+		// nRight = slider.eq(pos+1).addClass('o2R');
+		// nLeft = slider.eq(len).addClass('o2L');
+		posRight();
 		initialSlider();
+		setTimer();
 	}
 	
+	function setTimer(){
+		clearInterval(timer);
+		timer = setInterval(posRight, speed);
+	}
+
 	$(window).on({
 		resize: function(e){
 			initialSlider();
 		}
 	});			
-	setInterval(picRight, 3000);
+	
 	initial();
 	
 });
