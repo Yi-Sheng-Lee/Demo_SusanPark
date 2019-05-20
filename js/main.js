08626ee352e1bbd6577cadd40b4f5416b4361df5
Yi-Sheng-Lee <50228624+Yi-Sheng-Lee@users.noreply.github.com>
2019-05-20 21:29:41 +0800

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	var span = $('#header>div>div:nth-of-type(2)>nav>ul>li>div[data-decoration="corner"]>label>span');
 	var h3 = $('#header>div>div:nth-of-type(2)>nav>ul>li>div[data-layout=submenu]>section>label>h3');
-
 	var preObj1='';
 	var preObj2='';
 	
@@ -31,7 +30,7 @@ $(document).ready(function(){
 				$('[data-decoration="corner"]>div').removeAttr('style');
 				$('#header>div>div:nth-of-type(2)>nav>ul>li div[data-layout=submenu]').removeAttr('style');
 				$('#header>div>div:nth-of-type(2)>nav>ul>li ul').removeAttr('style');
-			}	
+			}
 
 		}
 	});
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 	var slider = $("#slider>div>section:nth-of-type(1)>figure");
 	var arrows = $('#slider>div>section:nth-of-type(2)>a');
-	var points = $('#slider>div>section:nth-of-type(3)>ul a');
+	var points = $('#slider>div:nth-of-type(2)>ul>li>a');
 
 
-	var nRight, nLeft, current, out;
+	var nRight, nLeft, current, out, currentM;
 	var speed = 2500;
 	var timer; //slide timer
 	var pos = 0; //active position
 	var len = slider.length - 1; //slides total number
-	var point;
+	//var point;
 
 	arrows.on({
 		click: function(e){
@@ -20,7 +20,7 @@ $(document).ready(function(){
 			switch($(this).attr("data-direction")){
 				case 'right':
 					posRight();
-					
+					picRight();
 					break;
 				case 'left':
 					posLeft();
@@ -32,9 +32,14 @@ $(document).ready(function(){
 
 	points.on({
 		click: function(e){
+			clearInterval(timer);
 			e.preventDefault();
-			point = $(this).parent().index();
-			console.log(point);
+			//console.log($(this).parent().index());
+			nLeft = pos;
+			pos = $(this).parent().index();
+			current = pos;
+			picRight();
+			setTimer();
 		}
 	});
 
@@ -47,7 +52,7 @@ $(document).ready(function(){
 		} 
 		current = pos;
 		nRight = (pos + 1 > len) ? 0 : pos + 1 ; 
-		picRight();
+		
 	}
 
 	function posLeft(){
@@ -58,13 +63,15 @@ $(document).ready(function(){
 		} 
 		current = pos;
 		nLeft = (pos - 1 < 0) ? len : pos - 1;
-		
 	}
 
 	function picRight(){
+		slider.removeAttr('class');
 		if(window.matchMedia("(max-width:600px)").matches){
+			points.parent().removeAttr('class');
 			slider.eq(nLeft).addClass('a2OL');
 			slider.eq(current).addClass('active or2A');
+			points.parent().eq(current).addClass('active');
 		} else{
 			slider.eq(out).addClass('l2O');
 			slider.eq(nLeft).addClass('a2L');
@@ -74,6 +81,7 @@ $(document).ready(function(){
 	}
 
 	function picLeft(){
+		slider.removeAttr('class');
 		slider.eq(out).addClass('r2O');
 		slider.eq(nRight).addClass('a2R');
 		slider.eq(current).addClass('active l2A');
@@ -83,6 +91,7 @@ $(document).ready(function(){
 	function initialSlider(){
 		if(window.matchMedia("(max-width:600px)").matches){
 			$('#slider>div>section:nth-of-type(1)').height($(this).width() * 1 * 0.69 + 16);
+			$('#header>div>div:nth-of-type(2)').height($(window).innerHeight() - 20);
 		}
 		  else if(window.matchMedia("(min-width:601px) and (max-width:1280px)").matches){
 			$('#slider>div>section:nth-of-type(1)').height($(this).width() * 0.7 * 0.69 + 16);
@@ -108,7 +117,7 @@ $(document).ready(function(){
 	
 	function setTimer(){
 		clearInterval(timer);
-		timer = setInterval(posRight, speed);
+		timer = setInterval(function(){posRight(); picRight();}, speed);
 	}
 
 	$(window).on({
@@ -117,8 +126,10 @@ $(document).ready(function(){
 			initialSlider();
 			if(window.matchMedia("(max-width:600px)").matches){
 				slider.removeClass('o2R o2L r2A l2A a2R a2L l2O r2O');
+				$('#header>div>div:nth-of-type(2)').height($(window).innerHeight());
 			} else{
 				slider.removeClass('or2A a2OL');
+				$('#header>div>div:nth-of-type(2)').removeAttr('style');
 			}
 			picRight();
 			setTimer();
